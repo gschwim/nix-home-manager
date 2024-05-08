bfba9d39e408ba087a488c134c636c4acbf91f8a
Greg Schwimer <schwim@bitrail.com>
2024-05-08 17:30:52 +0000

    continuing move to .sh. Cleanup.

--- lib.sh
+++ lib.sh
@@ -128,7 +128,7 @@ install_wezterm_config() {
 	cp ~/src/nix-home-manager/wezterm.lua ~/.wezterm.lua
 }
 
-_textout() {
+textout() {
     echo "$1"
     shift
     if [ "$*" = "" ]; then
@@ -140,7 +140,7 @@ _textout() {
 }
 
 ui_confirm() {
-    _textout "$GREEN$GREEN_UL" "$1"
+    textout "$GREEN$GREEN_UL" "$1"
 
     local prompt="[y/n] "
     echo -n "$prompt"
@@ -152,10 +152,70 @@ ui_confirm() {
             echo ""
             return 1
         else
-            _textout "$RED" "Sorry, I didn't understand. I can only understand answers of y or n"
+            textout "$RED" "Sorry, I didn't understand. I can only understand answers of y or n"
             echo -n "$prompt"
         fi
     done
     echo ""
     return 1
+}
+
+install_system() {
+
+	# clean out old things
+	clean_old
+
+	if [[ $step == 1 ]]; then
+		keep_going
+	fi
+
+	# prepare and/or update the os
+	if [[ `uname` == "Linux" ]]; then
+		prepare_or_update_debs
+	elif [[ `uname` == "Darwin" ]]; then
+		prepare_or_update_osx
+	else
+		print "Unknown OS. Stopping!"
+		return
+	fi
+
+	# set the shell to zsh
+	set_shell
+
+	if [[ $step == 1 ]]; then
+		keep_going
+	fi
+
+	# install nix and home-manager
+	install_nix
+
+	if [[ $step == 1 ]]; then
+		keep_going
+	fi
+}
+
+install_user() {
+	# configure home-manager
+	configure_home_manager
+
+	if [[ $step == 1 ]]; then
+		keep_going
+	fi
+
+	# install neovim config
+	install_or_update_dotfiles_nvim
+
+	if [[ $step == 1 ]]; then
+		keep_going
+	fi
+
+	# install pyenv
+	install_or_update_pyenv
+
+	if [[ $step == 1 ]]; then
+		keep_going
+	fi
+
+	# install wezterm config
+	install_wezterm_config
 }
--- zsh/install.zsh
+++ zsh/install.zsh
@@ -1,74 +1,16 @@
-###### run the workflow
+# import the lib.sh stuff
+. ./lib.sh
 
 export step=0
 
 print "WARNING: This will render any existing environment destroyed!"
-vared -p "Install the environment? (Y/y to proceed): " -c proceed
+if ! ui_confirm "Install the environment? (Y/y to proceed): "; then
+	echo "OK, stopping!"
 
-if [[ ! ${proceed:l} =~ "y" ]]; then
-	print "Quitting install." 
-	return
-fi
+
+
 
 # prepare and/or update the osl
 print "Beginning the install..."
 
-install_system() {
 
-	# clean out old things
-	clean_old
-
-	if [[ $step == 1 ]]; then
-		keep_going
-	fi
-
-	# prepare and/or update the os
-	if [[ `uname` == "Linux" ]]; then
-		prepare_or_update_debs
-	elif [[ `uname` == "Darwin" ]]; then
-		prepare_or_update_osx
-	else
-		print "Unknown OS. Stopping!"
-		return
-	fi
-
-	# set the shell to zsh
-	set_shell
-
-	if [[ $step == 1 ]]; then
-		keep_going
-	fi
-
-	# install nix and home-manager
-	install_nix
-
-	if [[ $step == 1 ]]; then
-		keep_going
-	fi
-}
-
-install_user() {
-	# configure home-manager
-	configure_home_manager
-
-	if [[ $step == 1 ]]; then
-		keep_going
-	fi
-
-	# install neovim config
-	install_or_update_dotfiles_nvim
-
-	if [[ $step == 1 ]]; then
-		keep_going
-	fi
-
-	# install pyenv
-	install_or_update_pyenv
-
-	if [[ $step == 1 ]]; then
-		keep_going
-	fi
-
-	# install wezterm config
-	install_wezterm_config
-}
