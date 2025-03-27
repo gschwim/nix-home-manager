b83486ca367580514b3c07f9645725f210dcc6c6
Greg Schwimer <schwim@bitrail.com>
2025-03-26 21:11:32 -0700

    flake-utils

--- flake.nix
+++ flake.nix
@@ -4,38 +4,41 @@
   inputs = {
     # Specify the source of Home Manager and Nixpkgs.
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
-  outputs = { self, nixpkgs, home-manager, ... }:
-    let
-      mkHome = { system, modules }: home-manager.lib.homeManagerConfiguration
-      {
-        pkgs = import nixpkgs { inherit system; };
-        modules = modules ++ [
-          ./home.nix
-        ];
-      };
-    in {
-      homeConfigurations = {
-        darwin-intel = mkHome {
-          system = "x86_64-darwin";
-          modules = [];
-        };
-        linux-x86 = mkHome {
-          system = "x86_64-linux";
-          modules = [];
-        };
-        osx-intel = mkHome {
-          # this is a test only
-          system = "x86_64-darwin";
-          modules = [
-            ./hosts/osx.nix
+  outputs = { self, nixpkgs, home-manager, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        mkHome = { system, modules }: home-manager.lib.homeManagerConfiguration
+        {
+          pkgs = import nixpkgs { inherit system; };
+          modules = modules ++ [
+            ./home.nix
           ];
         };
+      in {
+        homeConfigurations = {
+          darwin-intel = mkHome {
+            system = "x86_64-darwin";
+            modules = [];
+          };
+          linux-x86 = mkHome {
+            system = "x86_64-linux";
+            modules = [];
+          };
+          osx-intel = mkHome {
+            # this is a test only
+            system = "x86_64-darwin";
+            modules = [
+              ./hosts/osx.nix
+            ];
+          };
       };
-    };
+    }
+  );
 }
