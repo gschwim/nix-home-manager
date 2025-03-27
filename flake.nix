75d622d4c836c9bd4ee7d7017957247f6cf62da5
Greg Schwimer <schwim@bitrail.com>
2025-03-27 11:36:04 -0700

    reverted flake-utils out of flake.nix

--- flake.nix
+++ flake.nix
@@ -4,44 +4,38 @@
   inputs = {
     # Specify the source of Home Manager and Nixpkgs.
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
-  outputs = { self, nixpkgs, home-manager, flake-utils, ... }:
-
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        mkHome = { system, modules }: home-manager.lib.homeManagerConfiguration
-        {
-          system = system;
-          pkgs = import nixpkgs { inherit system; };
-          modules = modules ++ [
-            ./home.nix
+  outputs = { self, nixpkgs, home-manager, ... }:
+    let
+      mkHome = { system, modules }: home-manager.lib.homeManagerConfiguration
+      {
+        pkgs = import nixpkgs { inherit system; };
+        modules = modules ++ [
+          ./home.nix
+        ];
+      };
+    in {
+      homeConfigurations = {
+        darwin-intel = mkHome {
+          system = "x86_64-darwin";
+          modules = [];
+        };
+        linux-x86 = mkHome {
+          system = "x86_64-linux";
+          modules = [];
+        };
+        osx-intel = mkHome {
+          # this is a test only
+          system = "x86_64-darwin";
+          modules = [
+            ./hosts/osx.nix
           ];
         };
-      in {
-        homeConfigurations = {
-          darwin-intel = mkHome {
-            system = system;
-            # system = "x86_64-darwin";
-            modules = [];
-          };
-          linux-x86 = mkHome {
-            # system = "x86_64-linux";
-            modules = [];
-          };
-          osx-intel = mkHome {
-            # this is a test only
-            # system = "x86_64-darwin";
-            modules = [
-              ./hosts/osx.nix
-            ];
-          };
       };
-    }
-  );
+    };
 }
